38b3f47aca1f01f11514be7e69c4ba057edff8b5
EmmaK <emmaculatewkamau@gmail.com>
2022-03-17 12:37:01 +0300

    Add button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function readMore(){
-    var readMore = document.getElementById("read-more")
+    //var readMore = document.getElementById("read-more")
     var moreTxtOne = document.getElementById("more-txt-one")
     var moreTxtTwo = document.getElementById("more-txt-one")
+
+    console.log("meeee")
+
+    if(moreTxtOne.style.display === "none"){
+        moreTxtOne.style.display === "block"
+    }else{
+        moreTxtOne.style.display === "none"
+    }
 }
