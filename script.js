11d192b4507026039ec981d1cf2d45153d42d90e
EmmaK <emmaculatewkamau@gmail.com>
2022-03-17 12:16:00 +0300

    Add styling and start readmore function

---
+++ script.js
@@ -0,0 +1,5 @@
+function readMore(){
+    var readMore = document.getElementById("read-more")
+    var moreTxtOne = document.getElementById("more-txt-one")
+    var moreTxtTwo = document.getElementById("more-txt-one")
+}
